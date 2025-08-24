a16d3d2e7eee7e89a7b27b43ede83dc1f9e2bb82
JoshuaEntrata <kesselentrata@gmail.com>
2025-08-24 09:45:18 +0800

    Add dropdown in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,58 @@
+import { Link } from "react-router-dom";
+import { Avatar, Dropdown } from "antd";
+import {
+  UserOutlined,
+  SettingOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
 import styles from "../styles/components/Header.module.css";
 
 const Header = () => {
+  const items = [
+    {
+      key: "profile",
+      label: <Link to="/profile">Profile</Link>,
+      icon: <UserOutlined />,
+    },
+    {
+      key: "settings",
+      label: <Link to="/settings">Settings</Link>,
+      icon: <SettingOutlined />,
+    },
+    { type: "divider" },
+    {
+      key: "logout",
+      label: <Link to="/logout">Logout</Link>,
+      icon: <LogoutOutlined />,
+    },
+  ];
+  const username = "Joshua";
+
   return (
     <header className={styles.header}>
-      <div className={styles.header__logo}>
+      <div className={styles.logo}>
         <h1>Social Blogging App</h1>
       </div>
-      <nav className={styles.header__nav}>
+
+      <nav className={styles.navLinks}>
         <ul>
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/post">Post</a>
+            <Link to="/post">Post</Link>
           </li>
           <li>
-            <a href="/profile">Profile</a>
+            <Dropdown menu={{ items }} trigger={["click"]}>
+              <div className={styles.userMenu}>
+                <Avatar
+                  src="https://wallpapers.com/images/featured/rick-and-morty-pictures-b3e2pq02sb2fuvy3.jpg"
+                  alt="avatar"
+                  className={styles.avatar}
+                />
+                <span className={styles.username}>Hi, {username}</span>
+              </div>
+            </Dropdown>
           </li>
         </ul>
       </nav>
