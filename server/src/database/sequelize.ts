0a2720d266f371cee671f61fa0bb97c25fcc0285
JoshuaEntrata <kesselentrata@gmail.com>
2026-01-12 11:13:56 +0800

    Migrate from SQLite to PostgresSQL

--- server/src/database/sequelize.ts
+++ server/src/database/sequelize.ts
@@ -1,7 +1,23 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./db.sqlite",
-  logging: false,
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
 });
